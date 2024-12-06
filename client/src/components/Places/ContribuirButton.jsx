5a46c76b70645c53dfcbf409d549a7728a503208
3daniels3 <danidsf113@gmail.com>
2024-12-06 11:39:40 -0500

    detalles para presentacion

--- client/src/components/Places/ContribuirButton.jsx
+++ client/src/components/Places/ContribuirButton.jsx
@@ -2,10 +2,10 @@ export default function ContribuirButton({accion}) {
 
   return(
     <button
-      className="mt-8 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"
+      // className="mt-8 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg flex justify-center mx-auto"
       onClick={() => accion}
     >
-      Contribuir
+      {/* CONTRIBUIR */}
     </button>
   )
 }
